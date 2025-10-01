10facc7b413a243a58c206a4f38cd5a6b37695e3
Luis Santiago (Tonhão) <luis.santiago@techbiz.com.br>
2025-09-30 22:38:11 -0500

    test(users): create post users endpoint tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,26 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fastapi_zero.app import app
-
-
-def test_root_dev_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
-
+def test_root_dev_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá, Mundo!'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'tonhaodev',
+            'email': 'luis@tonhao.dev',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'tonhaodev',
+        'email': 'luis@tonhao.dev',
+        'id': 1,
+    }

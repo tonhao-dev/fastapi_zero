112484a7aecd1c78fb28aedf9e0f2fb6d1e11893
Luis Santiago (Tonhão) <luis.santiago@techbiz.com.br>
2025-09-30 22:37:51 -0500

    feat(app): create post users

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,24 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fastapi_zero.schemas import Message, UserDB, UserPublicSchema, UserSchema
 
 app = FastAPI()
 
 
-@app.get('/')
+database = []
+
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá, Mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublicSchema)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id

4ac3bdda336ba7fdb5a852408936c41f8190aa91
Luis Santiago (Tonhão) <luis.santiago@techbiz.com.br>
2025-09-30 22:37:31 -0500

    feat(schema): create user schemas

---
+++ fastapi_zero/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, EmailStr
+
+
+class Message(BaseModel):
+    message: str
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+
+class UserPublicSchema(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+
+
+class UserDB(UserSchema):
+    id: int

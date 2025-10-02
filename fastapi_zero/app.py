900a9510c996f51f150b861c9ece7a0e2a0b639b
Luis Santiago (Tonhão) <luis.santiago@techbiz.com.br>
2025-10-01 20:59:44 -0500

    feat(users): implement update user endpoint

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from fastapi_zero.schemas import (
     Message,
@@ -31,5 +31,16 @@ def create_user(user: UserSchema):
     user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
 
     database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.put('/users/{user_id}', response_model=UserPublicSchema)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='User not found')
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
 
     return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -39,3 +39,21 @@ def test_read_users(client):
             }
         ]
     }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'tonhas',
+            'email': 'luis.developer.ac@gmail.com',
+            'password': 'supersecret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'tonhas',
+        'email': 'luis.developer.ac@gmail.com',
+        'id': 1,
+    }
